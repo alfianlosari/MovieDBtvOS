f532922c80004d1843a81d24f755d236b24517b5
Alfian Losari <alfianlosari@gmail.com>
2019-03-23 10:29:15 +0700

    Initial Commit

---
+++ MovieDBTV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDBTV
+//
+//  Created by Alfian Losari on 23/03/19.
+//  Copyright © 2019 Alfian Losari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
